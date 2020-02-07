53dc6ce08146d1b0403ea3bebcf99b7ff6240098
NohaHossam123 <58139250+NohaHossam123@users.noreply.github.com>
2020-02-07 22:40:40 +0200

    new version of selecting data from table

--- selectTable.sh
+++ selectTable.sh
@@ -1,17 +1,20 @@
 function selectFromTable {
 	echo "Enter the table you want to display it : "
         read tableIsSelect
-       
-	if [ -z $tableIsSelect ]
-        then
-          echo "you must to enter the name of the table!"
-	elif [[ -f  "$databases/$dataBaseName/$tableIsSelect" ]]
+        if [ -z $tableIsSelect ]
 	then
+        echo "you must to enter the name of the table"
+        else
+          if [[ -f  "$databases/$dataBaseName/$tableIsSelect" ]]
+          then
           awk -F, '{if(NR!=1){print$1}}' $databases/$dataBaseName/$tableIsSelect.metadata | tr ['\n'] ['\t']
           echo $'\n'
-          cat $databases/$dataBaseName/$tableIsSelect | tr [','] [' ']         
-        else
-          echo "Sorry,this table doesn't exist!"
+          cat $databases/$dataBaseName/$tableIsSelect | tr [','] [' '] 
+          else
+          echo "the table you have entered does not exists please try again..."
+          fi
         fi
+	
 
 }
+
