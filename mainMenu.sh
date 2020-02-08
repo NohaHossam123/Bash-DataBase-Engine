ea51820fec6bcb6e7ff204325d9bf5c63106d91a
NohaHossam123 <58139250+NohaHossam123@users.noreply.github.com>
2020-02-08 22:46:04 +0200

    edit in main Menu

--- mainMenu.sh
+++ mainMenu.sh
@@ -14,10 +14,15 @@ fi
 
 
 function listDB {
-    #pwd
     if [ -d $databases ]
     then
-        ls $databases
+    #if [ -d $databases/$dataBaseName ]
+    if [ "$(ls -A $databases/$dataBaseName)" ]
+    then
+       ls $databases
+    else
+       echo "there aren't any databases to show..."
+    fi
     else
         echo "you don't have any file for databases!"
     fi
@@ -26,41 +31,48 @@ function listDB {
 function connectToDataBase {
     echo "please choose a database to connect : "
     read dataBaseName
+    if [ -z $dataBaseName ]
+    then
+      echo "you didn't choose any database....."
+    else
     if [ -d $databases/$dataBaseName ]
     then
         export dataBaseName
         export databases
         . ./secondMenu.sh
     else
-        echo "database not found please try again..."
+        echo "$dataBaseName  not found please try again..."
+    fi
     fi
 }
 
 function createDB {
-    #pwd
     if [ -d $databases ]
     then
-        cd $databases
-        read -p "enter name of database :" databaseName
-        if [ -d "$databaseName" ] &&  [ -z "$databaseName" ]
-        then
-            echo   "$databaseName is exist"
-        fi
+       #cd $databases
+       read -p "enter name of database :" databaseName
+    if [ -d "$databases/$databaseName" ] &&  [ -z "$databases/$databaseName" ]
+    then
+       echo   "$databaseName is exist"
+    fi
         
-        if [  -z "$databaseName"  ]
-        then
-            echo "you must enter database name!"
-        elif [ -d "$databaseName" ]
-        then
-            echo "$databaseName already exists please try again..."
-        else
-            mkdir $databaseName
-        fi
+    if [  -z "$databaseName"  ]
+    then
+       echo "you must enter database name....."
+    elif [ -d "$databases/$databaseName" ]
+    then
+       echo "$databaseName already exists please try again..."
+    elif [[ "$databaseName" =~ ^[0-9]+$ ]] #|| [[ "$databaseName" =~ ^[0-9Z]+$ ]]
+    then
+       echo "sorry,name of database must to be string only.."
+    else
+       mkdir $databases/$databaseName
+    fi
     else
         echo "you didn't in the right path!"
     fi
     pwd
-    cd ..
+    #cd ..
 }
 
 
